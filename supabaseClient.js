61042cb4310b024097f1a4f2f9a53042d27853bd
chash-code <panchalchash@gmail.com>
2026-03-08 15:28:27 +0530

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,8 +2,8 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  'https://kqalvxeigcevkxmgqgcf.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxYWx2eGVpZ2Nldmt4bWdxZ2NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NTY5MjIsImV4cCI6MjA4ODUzMjkyMn0.xcwZXK_mKWeO10RBMh3KXPRkxmcoH1zI2OJW4CTY6FQ'
 );
 
 module.exports = supabase;
